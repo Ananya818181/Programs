b12c61309fbabf80c8a937a144549a01a23f6e4a
Ananya <ananyasinghana3737@gmail.com>
2024-11-20 17:23:47 +0530

    Added new file to Programs repository

---
+++ linkedlist_create.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+} *head;
+
+void create(int n);
+void traverse();
+
+void create(int n) {
+    struct node *newNode, *temp;
+    int data, i;
+
+    head = (struct node*)malloc(sizeof(struct node));
+    if (head == NULL) {
+        printf("Unable to allocate memory\n");
+        exit(0);
+    } else {
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+        head->data = data;
+        head->next = NULL;
+        temp = head;
+
+        for (i = 2; i <= n; i++) {
+            newNode = (struct node*)malloc(sizeof(struct node));
+            if (newNode == NULL) {
+                printf("Unable to allocate memory\n");
+                break;
+            } else {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+                newNode->data = data;
+                newNode->next = NULL;
+                temp->next = newNode;
+                temp = newNode;
+            }
+        }
+    }
+}
+
+void traverse() {
+    struct node *temp;
+    temp = head;
+
+    if (head == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+
+    printf("The linked list elements are: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of nodes to be inserted: ");
+    scanf("%d", &n);
+
+    create(n);
+    traverse();
+
+    return 0;
+}
